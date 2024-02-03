c7c9a4e306fbd5d9b7fcdf7cbb49d5804565b293
Shikha <101914410+shikha046@users.noreply.github.com>
2024-02-03 11:50:36 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,117 @@
+<?php 
+
+	//include header file
+	include ('include/header.php');
+
+?>
+
+<style>
+	.size{
+		padding: 80px 0px;
+	}
+	img{
+		width: 300px;
+		height: 300px;
+	}
+	h2{
+		color: #e74c3c;
+	}
+	.white{
+		background-color: white;
+	}
+	p{
+		font-size: 18px;
+	}
+	.right{
+		float: right;
+	}
+	h1{
+		color: white;
+	}
+	.size{
+		min-height: 0px;
+		padding: 60px 0 40px 0;
+		
+	}
+
+</style>
+<div class="container-fluid red-background size">
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<p class="text-white text-center" style="padding: 10px 0 0 0;font-weight:bold" >The stories that inspires us</p>
+			<h1 class="text-center"style="color: white; font-weight: bold;padding: 10px 0 0 0;" >Bonded by Blood</h1>
+			<hr class="white-bar">
+		</div>
+	</div>
+</div>
+<div class="container-fluid size">
+	
+	<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="https://stanfordbloodcenter.org/wp-content/uploads/2021/06/Lily-Shih.jpeg" alt="Bonded By Blood" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h3 class="text-center">LILY’S STORY: DONOR AT LAST!</h3>
+			<hr class="red-bar">
+			<p>
+			Growing up, Lily Shih said she never really gave blood donation any thoughts. “I’m Chinese and it just wasn’t a big part of our culture,” she said. It wasn’t until adulthood when her mom was diagnosed with a rare neurological disease and needed transfusions of her own that Lily understood the importance of transfusion. From that time on, she was on a mission — albeit a long and difficult one — to donate blood.
+
+“I’d always been afraid to donate blood because I knew I had veins that were very hard to find,” she said.
+			</p>
+			<a href="donate.php" class="btn btn-danger btn-lg text-center center-aligned">
+							Read More</a>
+		</div>
+	</div>
+	</div>
+</div>
+
+<div class="container-fluid white size">
+	<div class="container ">
+	<div class="container">
+		<div class="row ">
+		<div class="col-md-6">
+			<h3 class="text-center">ERNEST’S STORY: “HOW DONATING BLOOD SAVED MY LIFE — TWICE”</h3>
+			<hr class="red-bar">
+			<p>
+			Ernest first really committed to giving blood in the early 1990s. Though he had donated one or two times in the past, it never really stuck until one day he heard on the news about a truck driver in Oakland who was in a serious accident and in immediate need of blood. He had never met this individual but, as someone who firmly believes in supporting others wherever and however he can, “felt [he] had to go help.”
+
+At his donation appointment at the county hospital, he saw a sign imploring individuals to register to give bone marrow. 
+		</p>
+		<a href="donate.php" class="btn btn-danger btn-lg text-center center-aligned">
+							Read More</a>	
+		</div>
+		<div ><img src="https://stanfordbloodcenter.org/wp-content/uploads/2021/05/200-donations.jpeg" alt="Bonded By Blood" class="rounded img-fluid float-right"></div>
+	</div>
+	</div>		
+</div>
+</div>
+	
+
+	<div class="container-fluid size">
+		<div class="container">
+		<div class="row">
+		<div class="col-md-6"><img src="https://stanfordbloodcenter.org/wp-content/uploads/2020/10/IMG_4657.jpeg" alt="Our Vission" class="rounded float-left img-fluid"></div>
+		<div class="col-md-6">
+			<h3 class="text-center">JULIE’S STORY: LOVING DAUGHTER, DEDICATED DONOR</h3>
+			<hr class="red-bar">
+			<p>
+			
+
+At just 19 years old, Julie Cruzal’s world was shaken when, for the first time that she could remember, it was her own hero — her mom — who needed saving.
+
+On that fateful day, Julie witnessed the terrifying consequences of a medical condition her mom was experiencing that impacted her blood supply, unbeknownst to her family at the time. By the time her mom was rushed to the hospital, she had lost nearly 50% of her total blood volume and needed an emergency transfusion.
+
+“It was really a wakeup call,” Julie said.
+			</p>
+			
+			<a href="donate.php" class="btn btn-danger btn-lg text-center center-aligned">
+							Read More</a>	
+		</div>
+	</div>
+	</div>
+	</div>
+<?php 
+
+	//include footer file
+	include ('include/footer.php');
+
+?>
---
+++ include/config.php
@@ -0,0 +1,4 @@
+<?php
+    $connection = mysqli_connect("localhost","root","","donatetheblood") or die("Database is not Conneted.".mysqli_connect_error());
+ 
+?>    
